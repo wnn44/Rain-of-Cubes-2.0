b0ba094c23aceec44f73672e4d8f79d654759955
Nikolay Voronin <125467426+wnn44@users.noreply.github.com>
2025-02-27 21:10:46 +0300

    Spawn bomb

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,6 +1,35 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    private Renderer _renderer;
+    private Color _initialColor = Color.red;
+    private float _minTime = 2.0f;
+    private float _maxTime = 5.0f;
+    private Rigidbody _rigidbody;
+
+    public event Action<Bomb> EndedLife;
+
+    public void Init(Vector3 position)
+    {
+        transform.position = position;
+        LiveDeley();
+    }
+
+    private void LiveDeley()
+    {
+        //_renderer.material.color = UnityEngine.Random.ColorHSV();
+        float delay = UnityEngine.Random.Range(_minTime, _maxTime);
+        StartCoroutine(Delay(delay));
+    }
+
+    private IEnumerator Delay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        EndedLife?.Invoke(this);
+    }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private Bomb _bombPrefab;
     [SerializeField] private SpawnPoint _startPoint;
     [SerializeField] private float _repeatRate = 0.3f;
     [SerializeField] private int _initialPoolSize = 5;
@@ -11,10 +12,12 @@ public class GameManager : MonoBehaviour
     private float _startPointMin = -10.0f;
     private float _startPointMax = 10.0f;
     private GenericSpawner<Cube> _cubeSpawner;
+    private GenericSpawner<Bomb> _bombSpawner;
 
     private void Start()
     {
         _cubeSpawner = new GenericSpawner<Cube>(_cubePrefab, _initialPoolSize);
+        _bombSpawner = new GenericSpawner<Bomb>(_bombPrefab, _initialPoolSize);
 
         StartCoroutine(SpawnCubes());
     }
@@ -31,11 +34,6 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void ActionOnGet(Cube cube)
-    {
-        cube.Init();
-    }
-
     private void TakeFromPool()
     {
         Cube cube = _cubeSpawner.Spawn(StartPoint(), transform.rotation);
@@ -43,12 +41,20 @@ public class GameManager : MonoBehaviour
 
         cube.EndedLife += OnRelease;
     }
+    
+    private void ActionOnGet(Cube cube)
+    {
+        cube.Init();
+    }
 
     private void OnRelease(Cube cube)
     {
+        Vector3 position = cube.transform.position;
         _cubeSpawner.Despawn(cube);
 
         cube.EndedLife -= OnRelease;
+
+        BombSpawn(position);
     }
 
     private Vector3 StartPoint()
@@ -59,4 +65,20 @@ public class GameManager : MonoBehaviour
 
         return new Vector3(x, y, z);
     }
+
+    private void BombSpawn(Vector3 position)
+    {
+        Bomb bomb = _bombSpawner.Spawn(StartPoint(), transform.rotation);
+
+        bomb.Init(position);
+        bomb.EndedLife += OnReleaseBomb;
+    }
+
+    private void OnReleaseBomb(Bomb bomb)
+    {
+        _bombSpawner.Despawn(bomb);
+
+        bomb.EndedLife -= OnReleaseBomb;
+    }
+
 }
