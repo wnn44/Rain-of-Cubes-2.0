00057a2fcf60a891c99fb8d4c47589040855b1d4
Nikolay Voronin <wnn@yandex.ru>
2025-02-28 17:00:49 +0300

    Bomb Spawner

--- Assets/Scripts/BombSpawner.cs
+++ Assets/Scripts/BombSpawner.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class BombSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Bomb _bombPrefab;
+    [SerializeField] private int _initialPoolSize = 5;
+
+    private GenericSpawner<Bomb> _bombSpawner;
+    private CubeSpawner _cubeSpawner;
+
+    private void Start()
     {
-        
+        _bombSpawner = new GenericSpawner<Bomb>(_bombPrefab, _initialPoolSize);
+        _cubeSpawner = new CubeSpawner();
+
+        //StartCoroutine(SpawnCubes());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        _cubeSpawner.CubeReturnedToPool += TakeFromPool;
+        Debug.Log("+");
+
     }
+
+    private void TakeFromPool(Vector3 spawnPoint)
+    {
+        Debug.Log("Spawn");
+        Bomb bomb = _bombSpawner.Spawn(spawnPoint, Quaternion.identity);
+
+
+    }
+
 }
--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -1,16 +1,19 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private Cube _cubePrefab;
-    [SerializeField] private SpawnPoint _startPoint;
+    [SerializeField] private SpawnPoint _spawnPoint;
     [SerializeField] private float _repeatRate = 0.3f;
     [SerializeField] private int _initialPoolSize = 5;
 
-    private float _startPointMin = -10.0f;
-    private float _startPointMax = 10.0f;
+    private float _spawnPointMin = -10.0f;
+    private float _spawnPointMax = 10.0f;
     private GenericSpawner<Cube> _cubeSpawner;
+
+    public event Action<Vector3> CubeReturnedToPool;
 
     private void Start()
     {
@@ -53,14 +56,14 @@ public class CubeSpawner : MonoBehaviour
 
         cube.EndedLife -= OnRelease;
 
-        //BombSpawn(position);
+        CubeReturnedToPool?.Invoke(position);
     }
 
     private Vector3 StartPoint()
     {
-        float y = _startPoint.transform.position.y;
-        float x = UnityEngine.Random.Range(_startPointMin, _startPointMax);
-        float z = UnityEngine.Random.Range(_startPointMin, _startPointMax);
+        float y = _spawnPoint.transform.position.y;
+        float x = UnityEngine.Random.Range(_spawnPointMin, _spawnPointMax);
+        float z = UnityEngine.Random.Range(_spawnPointMin, _spawnPointMax);
 
         return new Vector3(x, y, z);
     }
